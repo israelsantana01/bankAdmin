b2b8532db1d9825d7d8d7b860e9dbe211c71cade
israelsantana01 <israelofi2@gmail.com>
2020-06-09 17:14:12 -0300

    Crete Java project using Itellij IDEA

---
+++ src/Account.java
@@ -0,0 +1,46 @@
+public class Account {
+    private int agency;
+    private String owner;
+    private Double funds;
+    private int id;
+    private static int idCounter = 0;
+
+    public Account(int agency, String owner) {
+        this.agency = agency;
+        this.owner = owner;
+        this.funds = 0.0;
+        this.id = idCounter++;
+    }
+
+    public int getAgency() {
+        return agency;
+    }
+
+    public void setAgency(int agency) {
+        this.agency = agency;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public Double getFunds() {
+        return funds;
+    }
+
+    public void setFunds(Double funds) {
+        this.funds = funds;
+    }
+
+    public void deposit(Double value) {
+        this.funds += value;
+    }
+
+    public void withdraw(Double value) {
+        this.funds -= value;
+    }
+}
