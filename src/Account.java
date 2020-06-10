917972d319570e37a746bdcd7d9a5e809213b929
israelsantana01 <israelofi2@gmail.com>
2020-06-10 14:48:52 -0300

    Create the deposit function

--- src/Account.java
+++ src/Account.java
@@ -3,44 +3,64 @@ public class Account {
     private String owner;
     private Double funds;
     private int id;
-    private static int idCounter = 0;
+    private static int idCounter = 1;
 
-    public Account(int agency, String owner) {
+    public Account(int agency, String owner)
+    {
         this.agency = agency;
         this.owner = owner;
         this.funds = 0.0;
         this.id = idCounter++;
     }
 
-    public int getAgency() {
+    public int getAgency()
+    {
         return agency;
     }
 
-    public void setAgency(int agency) {
+    public void setAgency(int agency)
+    {
         this.agency = agency;
     }
 
-    public String getOwner() {
+    public String getOwner()
+    {
         return owner;
     }
 
-    public void setOwner(String owner) {
+    public void setOwner(String owner)
+    {
         this.owner = owner;
     }
 
-    public Double getFunds() {
+    public Double getFunds()
+    {
         return funds;
     }
 
-    public void setFunds(Double funds) {
+    public void setFunds(Double funds)
+    {
         this.funds = funds;
     }
 
-    public void deposit(Double value) {
+    public void deposit(Double value)
+    {
         this.funds += value;
     }
 
-    public void withdraw(Double value) {
+    public void withdraw(Double value)
+    {
         this.funds -= value;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Account{" +
+                "agency=" + agency +
+                ", owner='" + owner + '\'' +
+                ", funds=" + funds +
+                ", id=" + id +
+                '}';
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,112 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        Scanner nameInput = new Scanner(System.in);
+        Scanner valueInput = new Scanner(System.in);
+        ArrayList<Account> accounts = new ArrayList<Account>();
+        boolean run = true;
+        int option = 0;
 
-        System.out.println("-----------");
-        System.out.println("Menu");
+        while (run) {
+            System.out.println("--------------------------------------");
+            System.out.println("Menu");
+            System.out.println("1 - Abrir conta corrente");
+            System.out.println("2 - Depositar");
+            System.out.println("3 - Sacar");
+            System.out.println("1 - Consultar saldo");
+            System.out.println("1 - Contas com saldo zerado");
+            System.out.println("7 - Sair");
+            option = valueInput.nextInt();
 
+            if (option == 1 && accounts.size() < 10) {
+                boolean run2 = true;
+                while (run2) {
+
+                    System.out.println("Digite o seu nome: ");
+                    String name = nameInput.nextLine();
+                    System.out.println("Digite o número da agência");
+                    int agency = valueInput.nextInt();
+
+                    boolean validation = haveAgency(accounts, agency);
+
+                    if (validation) {
+                        Account user = new Account(agency, name);
+                        accounts.add(user);
+                        System.out.println("Conta criada com sucesso!");
+                        run2 = false;
+                    } else {
+                        System.out.println("Já existe uma conta com essa agência");
+                        System.out.println("Utilize outra combinação!");
+                        run2 = false;
+                    }
+                }
+            }
+
+            if (option == 2) {
+                boolean run3 = true;
+                System.out.println("Informe o número da agência: ");
+                int agency = valueInput.nextInt();
+                while (run3) {
+                    boolean verify = verification(accounts, agency);
+                    if (verify) {
+                        System.out.println("Quanto deseja depositar: ");
+                        Double value = valueInput.nextDouble();
+                        for (Account user : accounts) {
+                            if (user.getAgency() == agency && value > 0) {
+                                user.setFunds(value);
+                                System.out.println("Valor depositado com sucesso!");
+                                run3 = false;
+                                break;
+                            } else {
+                                System.out.println("Não foi possível realizar a operação...");
+                                run3 = false;
+                                break;
+                            }
+                        }
+                    } else {
+                        System.out.println("Não foi possível realizar a operação");
+                        run3 = false;
+                    }
+                }
+
+            }
+            switch (option) {
+                case 7:
+                    run = false;
+                    System.out.println(accounts + "\n");
+                    System.out.println("Sessão encerrada...");
+                    break;
+            }
+        }
 
 
     }
+
+    public static boolean haveAgency(ArrayList<Account> accounts, int agency)
+    {
+        boolean isValid = true;
+        for (Account user : accounts) {
+            if (user.getAgency() == agency) {
+                isValid = false;
+            }
+        }
+        return isValid;
+    }
+
+    public static boolean verification(ArrayList<Account> accounts, int agency)
+    {
+        boolean isValid = false;
+        for (Account user : accounts) {
+            if (user.getAgency() == agency) {
+                isValid = true;
+            }
+        }
+        return isValid;
+    }
 }
+
+
